90e7c2f2b1cc143b2177c81b26eb73d11b1f1e43
00Nhi00 <91705578+00Nhi00@users.noreply.github.com>
2021-12-15 21:36:25 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.nguyen;
+import com.nguyen.Paycheck;
+
+public class Main {
+	public static void main (String[] arg) {
+		Paycheck paycheck = new Paycheck();
+		System.out.println(String.format("Regular Pay: %f", paycheck.regularPay()));
+		paycheck.setHoursWorked(39.50);
+		paycheck.setPayRate(10);
+		System.out.println(String.format("Regular Pay: %f", paycheck.regularPay()));
+		paycheck.setHoursWorked(42);
+		System.out.println(String.format("Overtime Pay: %f", paycheck.overTimepay()));
+
+		String.format("Total Pay: %f", paycheck.totalPay());
+	}
+}
+
