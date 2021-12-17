bdd428433cbe233f5ac0babf5865d6852ec7517d
00Nhi00 <91705578+00Nhi00@users.noreply.github.com>
2021-12-16 20:04:46 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,14 @@ import com.nguyen.Paycheck;
 public class Main {
 	public static void main (String[] arg) {
 		Paycheck paycheck = new Paycheck();
-		System.out.println(String.format("Regular Pay: %f", paycheck.regularPay()));
+		System.out.println(String.format("Regular Pay: %.02f", paycheck.regularPay()));
 		paycheck.setHoursWorked(39.50);
 		paycheck.setPayRate(10);
-		System.out.println(String.format("Regular Pay: %f", paycheck.regularPay()));
+		System.out.println(String.format("Regular Pay: %.02f", paycheck.regularPay()));
 		paycheck.setHoursWorked(42);
-		System.out.println(String.format("Overtime Pay: %f", paycheck.overTimepay()));
-
-		String.format("Total Pay: %f", paycheck.totalPay());
+		System.out.println(String.format("Overtime Pay: %.02f", paycheck.overTimepay()));
+	
+		String.format("Total Pay: %.02f", paycheck.totalPay());
 	}
 }
 
